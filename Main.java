b1fd1cad69594b433b42e223a795db9f7d1f2667
Andrés Torres <cesarandrestorresbernal604@gmail.com>
2020-06-29 16:46:23 -0500

    forma alternativa de hacer la interfaz

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
-import views.*;
+import lupin.views.TableroJuego1;
+//import views.*;
 
 public class Main{
 
     public static void main(String[] args) {
-        View menu = new View();
-        menu.start();
+       // View menu = new View();
+        //menu.start();
+        TableroJuego1 tablero= new TableroJuego1();
+        tablero.setVisible(true);
     }
 }
