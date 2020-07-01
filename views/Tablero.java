6128d7c2a54b3c705a8e586a5002d767542b1fe3
spaezsuarez2 <lamperougelelouch74@gmail.com>
2020-07-01 11:08:40 -0500

    Colisiones en version dibujada

--- views/Tablero.java
+++ views/Tablero.java
@@ -3,10 +3,12 @@ package views;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 //import java.awt.Rectangle;
+import java.awt.geom.*;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.*;
+import java.awt.BasicStroke;
 
 public class Tablero extends JPanel implements KeyListener {
 
@@ -28,6 +30,7 @@ public class Tablero extends JPanel implements KeyListener {
 
     public void drawMap(Graphics2D lapiz) {
         lapiz.setColor(Color.BLACK);
+        lapiz.setStroke(new BasicStroke(3));
         lapiz.drawLine(0, 300, 540, 300);
         lapiz.drawLine(540, 0, 540, 500);
 
@@ -43,20 +46,30 @@ public class Tablero extends JPanel implements KeyListener {
 
     public void colisiones() {
 
+        Rectangle2D areaPersonaje = new Rectangle2D.Double(personaje.getX(),personaje.getY(),30,30);
+
+        if(areaPersonaje.intersectsLine(0, 300, 540, 300)){
+            this.personaje.setLocation(personaje.getX(), personaje.getY()-10);
+        }
+
+        if(areaPersonaje.intersectsLine(540, 0, 540, 500)){
+            this.personaje.setLocation(personaje.getX()-10, personaje.getY());
+        }
+
         if(this.personaje.getX() > this.getBounds().getMaxX()){
-            this.personaje.setLocation(this.getX()-10, this.getY());
+            this.personaje.setLocation(personaje.getX()-30, personaje.getY());
             
         }
         if(this.personaje.getX() < this.getBounds().getMinX()){
-            this.personaje.setLocation(this.getX()+10, this.getY());
+            this.personaje.setLocation(personaje.getX()+10, personaje.getY());
             
         }
         if(this.personaje.getY() > this.getBounds().getMaxY()){
-            this.personaje.setLocation(this.getX(), this.getY()-10);
+            this.personaje.setLocation(personaje.getX(), personaje.getY()-30);
             
         }
         if(this.personaje.getY() < this.getBounds().getMinY()){
-            this.personaje.setLocation(this.getX(), this.getY()+10);
+            this.personaje.setLocation(personaje.getX(), personaje.getY()+10);
             
         }
         for (int x = 0; x <= 540; x++) {
